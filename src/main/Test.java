5042aa61955b8d91922c287b6cf6fca8a20630e6
YosefRafaelJoshua <13522133@std.stei.itb.ac.id>
2024-05-25 18:40:19 +0700

    fix: KartuProduk

--- src/main/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test{
-    public static void main(String[] args) {
-        KartuBijiJagung jagung = new KartuBijiJagung();
-        KartuAccelerate acc = new KartuAccelerate();
-        System.out.println(jagung.getUmur());
-        System.out.println(jagung.getUmur_harvest());
-
-    }
-}
